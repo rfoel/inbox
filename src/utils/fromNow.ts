c1356e1e6697f2597fbba178a3b05efa0c2bd75b
Rafael Franco <rafaelfr@outlook.com>
2024-03-15 23:31:57 -0300

    Display different date format when in a different year

--- src/utils/fromNow.ts
+++ src/utils/fromNow.ts
@@ -4,6 +4,10 @@ import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
 export const fromNow = (date: number) => {
+	if (!dayjs().isSame(date, "year")) {
+		return dayjs(date).format("DD/MM/YYYY");
+	}
+
 	if (dayjs().diff(date, "days") > 7) {
 		return dayjs(date).format("ddd DD/MM");
 	}
