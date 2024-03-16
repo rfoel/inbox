db209cbd89d87ae598c20a9c406366373668a310
Rafael Franco <rafaelfr@outlook.com>
2024-03-15 23:52:14 -0300

    Group recipients by domain

--- src/components/Recipients.tsx
+++ src/components/Recipients.tsx
@@ -25,38 +25,61 @@ export const RecipientsView = () => {
 		}
 	}, [query.data]);
 
+	const addresses =
+		query.data?.reduce(
+			(acc, recipient) => {
+				const [username, domain] = recipient.address.split("@");
+				if (!acc[domain]) {
+					acc[domain] = [];
+				}
+				acc[domain].push(username);
+				return acc;
+			},
+			{} as { [key: string]: string[] },
+		) ?? {};
+
 	return (
 		<div className="flex flex-col bg-white rounded-xl w-[256px] min-w-[256px]">
 			<h1 className="text-2xl font-black p-4">Recipients</h1>
 
-			{query.data?.length ? (
+			{Object.keys(addresses).length ? (
 				<div className="flex flex-col overflow-y-scroll overflow-x-hidden">
-					{query.data
-						.sort(
-							(a, b) =>
-								a.name.localeCompare(b.name) ||
-								a.address.localeCompare(b.address),
-						)
-						.map((recipient) => (
-							<button
-								key={recipient.address}
-								className={cn(
-									"p-4 hover:bg-green-100 w-full border-l-4 border-transparent text-left flex items-center gap-2",
-									{
-										"bg-green-50 border-green-200":
-											recipient.address === appContext.state.recipient,
-									},
-								)}
-								onClick={() => handleClick(recipient.address)}
-								type="button"
-							>
-								<div>{recipient.address}</div>
-								{recipient.unread > 0 ? (
-									<div className="flex items-center justify-center bg-red-500 text-white h-5 w-5 rounded-full text-xs">
-										{recipient.unread}
-									</div>
-								) : null}
-							</button>
+					{Object.entries(addresses)
+						.sort(([a], [b]) => a.localeCompare(b))
+						.map(([domain, usernames]) => (
+							<div>
+								<div className="font-bold text-sm p-4">{domain}</div>
+								{usernames.map((username) => {
+									const recipient = query.data?.find(
+										({ address }) => address === `${username}@${domain}`,
+									);
+									if (!recipient) {
+										return null;
+									}
+									return (
+										<button
+											key={`${username}@${domain}`}
+											className={cn(
+												"p-4 hover:bg-green-100 w-full border-l-4 border-transparent text-left flex items-center gap-2",
+												{
+													"bg-green-50 border-green-200":
+														`${username}@${domain}` ===
+														appContext.state.recipient,
+												},
+											)}
+											onClick={() => handleClick(`${username}@${domain}`)}
+											type="button"
+										>
+											<div>{username}</div>
+											{recipient.unread > 0 ? (
+												<div className="flex items-center justify-center bg-red-500 text-white h-5 w-5 rounded-full text-xs">
+													{recipient.unread}
+												</div>
+											) : null}
+										</button>
+									);
+								})}
+							</div>
 						))}
 				</div>
 			) : null}
